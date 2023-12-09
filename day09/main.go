95c2ab33ee9b173a03650dbf799cb7a0961f47e3
Jon Pomeroy <earl.pomeroy42@gmail.com>
2023-12-09 00:20:08 -0700

    Day 9, Part One solution

---
+++ day09/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type History struct {
+	sequence []int
+	base     *History
+}
+
+func (h *History) addBase() {
+	done := true
+	base := History{}
+
+	for i := 0; i < len(h.sequence)-1; i++ {
+		first := h.sequence[i]
+		second := h.sequence[i+1]
+
+		num := second - first
+
+		if num != 0 {
+			done = false
+		}
+
+		base.sequence = append(base.sequence, num)
+	}
+
+	h.base = &base
+
+	if !done {
+		base.addBase()
+	}
+}
+
+func (h *History) extrapolate() int {
+	for {
+		if h.base != nil {
+			num := h.base.extrapolate()
+			return h.sequence[len(h.sequence)-1] + num
+		} else {
+			return 0
+		}
+	}
+}
+
+func (h History) print() {
+	fmt.Printf("%v\n\t", h.sequence)
+
+	if h.base != nil {
+		h.base.print()
+	} else {
+		fmt.Println("")
+	}
+}
+
+var HistoryList = []History{}
+
+func makeHistory(line string) {
+	h := History{}
+
+	for _, s := range strings.Split(line, " ") {
+		num, _ := strconv.Atoi(s)
+		h.sequence = append(h.sequence, num)
+	}
+
+	h.addBase()
+
+	HistoryList = append(HistoryList, h)
+}
+
+func main() {
+	args := os.Args[1:]
+
+	if len(args) < 1 {
+		fmt.Println("Missing input file")
+		return
+	}
+
+	var filename = args[0]
+
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		var line = scanner.Text()
+		makeHistory(line)
+	}
+
+	result := 0
+
+	for _, h := range HistoryList {
+		result += h.extrapolate()
+		// h.print()
+	}
+
+	fmt.Println("History total: ", result)
+}
